6bca5e0d9104e1ea7b937fdacc95abcaa5caab51
Gustavo <gustavobrandao31@outlook.com>
2021-11-15 12:50:41 -0300

    Ajustes no cadastro

--- js/index.js
+++ js/index.js
@@ -11,3 +11,23 @@ function prancheta_login(){
         setTimeout(() => { document.getElementById("form_login").style.display = "None"; }, 200);
     }
 }
+
+function cadastro_user(alvo){
+    // 0 -> Login, 1 -> Cadastro
+
+    if(alvo){
+        document.getElementById("prancheta_cadastro").style.animation = "aparece_bloco .2s";
+        document.getElementById("prancheta_cadastro").style.display = "Block";
+
+        document.getElementById("prancheta_login").style.animation = "esconde_bloco .2s";
+        
+        setTimeout(() => { document.getElementById("prancheta_login").style.display = "None"; }, 200);
+    }else{
+        document.getElementById("prancheta_login").style.animation = "aparece_bloco .2s";
+        document.getElementById("prancheta_login").style.display = "Block";
+
+        document.getElementById("prancheta_cadastro").style.animation = "esconde_bloco .2s";
+        
+        setTimeout(() => { document.getElementById("prancheta_cadastro").style.display = "None"; }, 200);
+    }
+}
