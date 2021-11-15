d2a9b544bbd2408689cc41bbcbbe86faadf70042
Gustavo <gustavobrandao31@outlook.com>
2021-11-15 12:41:02 -0300

    ajustes com o css

--- js/index.js
+++ js/index.js
@@ -2,8 +2,12 @@ function prancheta_login(){
     
     alvo = document.getElementById("form_login");
 
-    if(alvo.style.display !== "block")
+    if(alvo.style.display !== "block"){
+        document.getElementById("form_login").style.animation = "aparece_bloco .2s";
         document.getElementById("form_login").style.display = "Block";
-    else
-        document.getElementById("form_login").style.display = "None";
+    }else{
+        document.getElementById("form_login").style.animation = "esconde_bloco .2s";
+        
+        setTimeout(() => { document.getElementById("form_login").style.display = "None"; }, 200);
+    }
 }
